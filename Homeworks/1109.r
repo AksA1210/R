76d15b10260039c9f7c887a92e0da5fbf8e38986
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-09-12 16:20:17 +0530

    Create 1109.r

---
+++ Homeworks/1109.r
@@ -0,0 +1,25 @@
+square <- function(n){
+  n <- as.integer(n)
+  print(paste("Square of the number : ",n*n))
+}
+n <- readline("Enter the number : ")
+square(n)
+
+name_len <-function(name){
+  print(paste("Length of the name : ",nchar(name)))
+}
+name_len("Akshadha")  
+
+func1 <- function(vr) {
+  #v<-c(1,2,3,4)
+  sum = 0
+  for (i in 1:length(v)) { 
+  sum = sum+v[i]
+  }
+  print(paste("Sum : ",sum))
+  print(paste("Maximum : ",max(v)))
+  print(paste("Minimum : ",min(v)))
+}  
+#x <- scan()
+#func1(x)
+func1(c(1,2,3,4))
