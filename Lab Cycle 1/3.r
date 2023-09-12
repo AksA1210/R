29397babe9c4e8d9f6613efa664246eb28678793
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-09-12 16:18:18 +0530

    Create 3.r

---
+++ Lab Cycle 1/3.r
@@ -0,0 +1,14 @@
+#Data validation
+student_records <- function(names,ages,grades){
+  for (i in 1:length(ages)){
+    if (ages[i] < 0){
+      print("Age is not positive !!!")
+    }else{
+      print("Age is validated")
+    }
+  }
+}
+names <- c("Lily","Mabel","Annie")
+ages <- c(18,17,19)
+grades <- c("A","B","A")
+student_records(names,ages,grades)
