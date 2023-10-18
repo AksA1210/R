e95bf60cb9262b8691f63a06f0c77b7ce103313d
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-18 23:34:03 +0530

    Update and rename Palindrome.r to q8.R

---
+++ Lab Cycle 1/q8.R
@@ -0,0 +1,17 @@
+palindrome_check <- function(x1){
+  string_split<- strsplit(x1,NULL)[[1]]
+  
+  rev_word = rev(string_split)
+  reversed_string <-paste(rev(string_split),collapse="")
+  cat("++++++++++++++++++++++++++++++++++++++",rev_word)
+  if(x1==reversed_string)
+  {
+    print("Palindrome")
+  }else{
+    print("Not a palindrome")
+  }
+  
+}
+print("***PALINDROME CHECKER***")
+x1<-readline(prompt = "Enter a word : ")
+palindrome_check(x1)
