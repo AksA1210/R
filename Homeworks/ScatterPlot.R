a1b461d1b714bb276a0e0ff97a12d10fae4a71eb
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-25 13:12:46 +0530

    Create ScatterPlot.R

---
+++ Homeworks/ScatterPlot.R
@@ -0,0 +1,4 @@
+#Customised scatter plot
+#Create a scatterplot of the sepal length and petal length variables in the iris dataset using the plot function? Add appropriate labels and title to the plot.Save the plot as a high-resolution image file.
+#Create a scatterplot of the mpg and disp variables in the mtcars dataset.Use different colors to represent the cyl variable and add a smooth line to show the trend.Add appropriate labels,title and legend to the plot.
+
