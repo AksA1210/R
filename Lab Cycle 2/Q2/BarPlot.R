5414e7a1110c73b9a3554e024cdf041058310ef8
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-26 22:49:15 +0530

    Update BarPlot.R

--- Lab Cycle 2/Q2/BarPlot.R
+++ Lab Cycle 2/Q2/BarPlot.R
@@ -1,1 +1,15 @@
+mtcars_data <- read.csv("D:/Aks/CUSAT/Semester V/R for Data Science/Lab/mtcars.csv")
+
+head(mtcars_data)
+
+mtcars_data$cyl <- as.character(mtcars_data$cyl)
+
+table_data <- xtabs(~ am + cyl, data = mtcars_data)
+
+barplot(table_data, beside = TRUE, col = c("green", "yellow"),
+        xlab = "Number of Cylinders", ylab = "Count",
+        main = "Cylinders vs. Transmission Type")
+
+
+legend("top", legend = unique(mtcars_data$am), fill = c("green", "yellow"), title = "Transmission Type")
 
