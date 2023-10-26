b070dcd2ab7d2075721c60953d3bce1c7c36d39f
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-26 22:04:32 +0530

    Update ScatterPlot.R

--- Lab Cycle 2/ScatterPlot.R
+++ Lab Cycle 2/ScatterPlot.R
@@ -1,1 +1,2 @@
-#Create a scatter plot
+#Create a scatterplot of the Sepal.Length and Petal.Length variables in the iris dataset using the plot function? Add appropriate labels and title to the plot.
+#Save the plot as a high-resolution image file.
