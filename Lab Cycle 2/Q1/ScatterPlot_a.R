16429335fb86ac7fb77341c65efe6937fb303948
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-10-26 22:33:40 +0530

    Update and rename Lab Cycle 2/ScatterPlot_a.R to Lab Cycle 2/Q1/ScatterPlot_a.R

--- Lab Cycle 2/Q1/ScatterPlot_a.R
+++ Lab Cycle 2/Q1/ScatterPlot_a.R
@@ -1,2 +1,8 @@
 #Create a scatterplot of the Sepal.Length and Petal.Length variables in the iris dataset using the plot function? Add appropriate labels and title to the plot.
 #Save the plot as a high-resolution image file.
+library(datasets)
+iris
+head(iris)
+plot(iris$Sepal.Length, iris$Petal.Length, 
+     xlab = "Sepal Length", ylab = "Petal Length",
+     main = "Scatterplot of Sepal Length vs Petal Length",pch=16,cex=0.75)
