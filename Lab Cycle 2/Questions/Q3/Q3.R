8a5f1176051c6b37abc632b26644b4da46578109
Akshadha A <95520592+AksA1210@users.noreply.github.com>
2023-12-14 09:57:41 +0530

    Update and rename Q1.R to Q3.R

--- Lab Cycle 2/Questions/Q3/Q3.R
+++ Lab Cycle 2/Questions/Q3/Q3.R
@@ -1,4 +1,4 @@
-#Q1: Create a bar plot of the number of cylinders (cyl) in the mtcars dataset. Use different colors to represent the transmission type (am). Add appropriate title, labels, and legend to the plot.
+#Q: Create a bar plot of the number of cylinders (cyl) in the mtcars dataset. Use different colors to represent the transmission type (am). Add appropriate title, labels, and legend to the plot.
 # Load the ggplot2 library
 library(ggplot2)
 
